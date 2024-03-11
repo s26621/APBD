1a89dc03113b9cdd1e75a7a17e35c4715bc74797
s26621 <S26621@pjwstk.edu.pl>
2024-03-11 11:43:19 +0100

    Suma tablicy

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        int[] tablica = { 1, 2, 3, 4 };
+        Console.WriteLine("Suma tablicy to: " + SumaTablicy(tablica));
+        Console.WriteLine("Srednia tablicy to: " + SredniaTablicy(tablica));
+        Console.WriteLine("Maks tablicy to: " + MaksTablicy(tablica));
+    }
+    
+    public static int SumaTablicy(int[] tablica)
+    {
+        int suma = 0;
+        foreach (int val in tablica)
+        {
+            suma += val;
+        }
+
+        return suma;
+    }
+    public static int SredniaTablicy(int[] tablica)
+    {
+        int suma = SumaTablicy(tablica);
+        return suma / tablica.Length;
+    }
+    public static int MaksTablicy(int[] tablica)
+    {
+        int min = tablica[0];
+        foreach (int val in tablica)
+        {
+            if (val < min) min = val;
+        }
+
+        return min;
+    }
+}
